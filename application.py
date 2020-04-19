e1417d439952653326af0a5fbbdf346980eb1f04
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-19 22:45:20 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+conn = mysql.connector.connect(user='myadmin@mydemoserver',
+                                   password='yourpassword',
+                                   database='quickstartdb',
+                                   host='mydemoserver.mysql.database.azure.com',
+                                   ssl_ca='/var/www/html/BaltimoreCyberTrustRoot.crt.pem')
+except mysql.connector.Error as err:
+    print(err)
+
 @app.route("/")
 def hello():
     return "Hello World!"
