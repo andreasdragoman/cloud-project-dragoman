121077dc022fedb2f673e6e50c126665fc81ae1f
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-20 22:29:37 +0300

    Update faceservice.py

--- faceservice.py
+++ faceservice.py
@@ -1,28 +1,33 @@
-# import asyncio
-# import io
-# import glob
 import os
-# import sys
-# import time
-# import uuid
-# import requests
-# from urllib.parse import urlparse
-# from io import BytesIO
-# from PIL import Image, ImageDraw
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
-# from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
 
 KEY = 'bb170de71ae24dfa8f17d2a472215bfe'
 ENDPOINT = 'https://cloud-project-face-service.cognitiveservices.azure.com/'
 
 def getFaceInfoFromURL():
-  face_detected = False
-  url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
-  face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
-  single_face_image_url = url
-  single_image_name = os.path.basename(single_face_image_url)
-  detected_faces = face_client.face.detect_with_url(url=single_face_image_url)
-  if not detected_faces:
-    return None
-  return detected_faces
+  # face_detected = False
+  # url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
+  # face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+  # single_face_image_url = url
+  # single_image_name = os.path.basename(single_face_image_url)
+  # detected_faces = face_client.face.detect_with_url(url=single_face_image_url)
+  # if not detected_faces:
+  #   return None
+  # return detected_faces
+  # replace <My Endpoint String> with the string from your endpoint URL
+  face_api_url = 'https://cloud-project-face-service.cognitiveservices.azure.com/face/v1.0/detect'
+  
+  image_url = 'https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg'
+  
+  headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+  
+  params = {
+      'returnFaceId': 'true',
+      'returnFaceLandmarks': 'false',
+      'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+  }
+  
+  response = requests.post(face_api_url, params=params,
+                           headers=headers, json={"url": image_url})
+  print(json.dumps(response.json()))
