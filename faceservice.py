66227f6ecff3bd060fe2447f3f49c26213a2a3d2
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-20 21:49:45 +0300

    Update faceservice.py

--- faceservice.py
+++ faceservice.py
@@ -13,12 +13,3 @@ from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 # from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
 
-KEY =  os.environ['FACE_SUBSCRIPTION_KEY'] # 'bb170de71ae24dfa8f17d2a472215bfe'
-ENDPOINT = os.environ['FACE_ENDPOINT'] # 'https://cloud-project-face-service.cognitiveservices.azure.com/'
-
-def getFacesInfoFromURL():
-  # global KEY
-  # global ENDPOINT
-  # face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
-  return 'kk'
-
