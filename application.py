f5ecb2664074e12123bea9feadae33e5a412a0ec
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-20 13:35:59 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,13 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-conn = mysql.connector.connect(user='DragomanDbUser@cloud-project-serverdb',
-                                   password='Portocale1',
-                                   database='cloudcomputing',
-                                   host='cloud-project-serverdb.mysql.database.azure.com')
-except mysql.connector.Error as err:
-    print(err)
-
 
 @app.route("/")
 def hello():
