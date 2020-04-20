d796a0abcba3a5cfecf37c5308910bf6c3d1bae6
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-20 14:32:12 +0300

    Update database.py

--- database.py
+++ database.py
@@ -23,10 +23,25 @@ except mysql.connector.Error as err:
 else:
   cursor = conn.cursor()
 
-  
-def getTestRecords():
-  # Read data
-  cursor.execute("SELECT * FROM test;")
-  rows = cursor.fetchall()
-  return rows
+  # Drop previous table of same name if one exists
+  cursor.execute("DROP TABLE IF EXISTS inventory;")
+  print("Finished dropping table (if existed).")
+
+  # Create table
+  cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
+  print("Finished creating table.")
+
+  # Insert some data into table
+  cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("banana", 150))
+  print("Inserted",cursor.rowcount,"row(s) of data.")
+  cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("orange", 154))
+  print("Inserted",cursor.rowcount,"row(s) of data.")
+  cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("apple", 100))
+  print("Inserted",cursor.rowcount,"row(s) of data.")
+
+  # Cleanup
+  conn.commit()
+  cursor.close()
+  conn.close()
+  print("Done.")
   
