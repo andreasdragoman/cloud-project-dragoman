151bd24c2cd909fd53498cc525a9c14d406c76ad
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-20 18:45:28 +0300

    Update face_service.py

--- face_service.py
+++ face_service.py
@@ -9,6 +9,6 @@ import os
 # from urllib.parse import urlparse
 # from io import BytesIO
 # from PIL import Image, ImageDraw
-from azure.cognitiveservices.vision.face import FaceClient
+# from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 # from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
