c5714852b0608fe00a9701cdb0f2fc61f3b5bfeb
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-20 20:25:04 +0300

    Update database.py

--- database.py
+++ database.py
@@ -9,68 +9,79 @@ config = {
   'database':'cloudcomputing'
 }
 
-is_connected = False
+# is_connected = False
 
 # Construct connection string
-try:
-   conn = mysql.connector.connect(**config)
-   print("Connection established")
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with the user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  is_connected = True
-  cursor = conn.cursor()
+# try:
+#    conn = mysql.connector.connect(**config)
+#    print("Connection established")
+# except mysql.connector.Error as err:
+#   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#     print("Something is wrong with the user name or password")
+#   elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#     print("Database does not exist")
+#   else:
+#     print(err)
+# else:
+#   is_connected = True
+#   cursor = conn.cursor()
 
 
 def createTables():
-  if is_connected == True:
+  try:
+    conn = mysql.connector.connect(**config)
+  except mysql.connector.Error as err:
+    return False
+  else:
+    cursor = conn.cursor()
     cursor.execute("DROP TABLE IF EXISTS inventory;")
     cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
-    conn.commit()
+    cleanUpConnection()
     return True
-  else:
-    return False
   
 
 def insertItemInInventory(item_name, item_quantity):
-  if is_connected == True:
-    cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", (item_name, item_quantity))
-    conn.commit()
-    return True
-  else:
+  try:
+    conn = mysql.connector.connect(**config)
+  except mysql.connector.Error as err:
     return False
+  else:
+    cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", (item_name, item_quantity))
+    cleanUpConnection()
   
   
 def getInventory():
-  if is_connected == True:
+  try:
+    conn = mysql.connector.connect(**config)
+  except mysql.connector.Error as err:
+    return null
+  else:
     cursor.execute("SELECT * FROM inventory;")
     rows = cursor.fetchall()
+    cleanUpConnection()
     return rows
-  else:
-    return null
   
   
 def updateInventoryItem(item_id, item_name, item_quantity):
-  if is_connected == True:
-    cursor.execute("UPDATE inventory SET name = %s, quantity = %s WHERE id = %d;", (item_name, item_quantity, item_id))
-    conn.commit()
-    return True
-  else:
+  try:
+    conn = mysql.connector.connect(**config)
+  except mysql.connector.Error as err:
     return False
+  else:
+    cursor.execute("UPDATE inventory SET name = %s, quantity = %s WHERE id = %d;", (item_name, item_quantity, item_id))
+    cleanUpConnection()
+    return True
   
   
 def deleteInventoryItem(item_id):
-  if is_connected == True:
-    cursor.execute("DELETE FROM inventory WHERE id=%d;", (item_id))
-    conn.commit()
-    return True
-  else:
+  try:
+    conn = mysql.connector.connect(**config)
+  except mysql.connector.Error as err:
     return False
+  else:
+    cursor.execute("DELETE FROM inventory WHERE id=%d;", (item_id))
+    cleanUpConnection()
+    return True
   
   
 def cleanUpConnection():
