149fbe923163172b1972aada0bce32224cb74138
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-21 02:31:07 +0300

    Update application.py

--- application.py
+++ application.py
@@ -5,17 +5,7 @@ from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
-def hello():
-  results = db.getInventory()
-  #return str(results[0])
-  results = fs.getFaceInfoFromURL()
-  # if results is not None:
-  #   return str(results)
-  return render_template("home.html")
-
-
-@app.route("/", methods=["POST"])
+@app.route("/", methods=['GET', 'POST'])
 def hello():
   results = db.getInventory()
   #return str(results[0])
