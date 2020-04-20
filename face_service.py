96140b367a61d0347fef41c7014971747545a532
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-20 19:51:14 +0300

    Delete face_service.py

--- face_service.py
+++
@@ -1,2 +0,0 @@
-KEY = "bb170de71ae24dfa8f17d2a472215bfe" # os.environ['FACE_SUBSCRIPTION_KEY']
-ENDPOINT = "https://cloud-project-face-service.cognitiveservices.azure.com/" # os.environ['FACE_ENDPOINT']
