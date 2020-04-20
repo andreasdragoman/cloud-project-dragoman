59607e4f066bd2532c4999564611a7d8cbd5860f
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-20 19:28:01 +0300

    Update face_service.py

--- face_service.py
+++ face_service.py
@@ -1,1 +1,4 @@
 import os
+
+KEY = "bb170de71ae24dfa8f17d2a472215bfe" # os.environ['FACE_SUBSCRIPTION_KEY']
+ENDPOINT = "https://cloud-project-face-service.cognitiveservices.azure.com/" # os.environ['FACE_ENDPOINT']
