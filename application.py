afed1af22579211008654478a57d98b498ae38df
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-21 01:35:28 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,15 @@
 import database as db
 import faceservice as fs
 
-from flask import Flask
+from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello():
   results = db.getInventory()
   #return str(results[0])
   results = fs.getFaceInfoFromURL()
-  if results is not None:
-    return str(results)
+  # if results is not None:
+  #   return str(results)
+  return render_template("home.html")
