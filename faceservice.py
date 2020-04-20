b47fc8d90fe9c921be64e6dd51fa3aea6c801727
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-20 21:09:17 +0300

    Update faceservice.py

--- faceservice.py
+++ faceservice.py
@@ -1,4 +1,18 @@
-test = 'test'
+# import asyncio
+# import io
+# import glob
+import os
+# import sys
+# import time
+# import uuid
+# import requests
+# from urllib.parse import urlparse
+# from io import BytesIO
+# from PIL import Image, ImageDraw
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+# from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
+
 
 def getVar():
   return 'test1'
