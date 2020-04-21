e74b69bf24f10ccb2d8aad175a436fa32c325287
andreasdragoman <andreas_dd@yahoo.ro>
2020-04-21 16:30:06 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,30 +1,13 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-# Obtain connection string information from the portal
+
 config = {
   'host':'cloud-project-serverdb.mysql.database.azure.com',
   'user':'DragomanDbUser@cloud-project-serverdb',
   'password':'Portocale1',
   'database':'cloudcomputing'
 }
-
-# is_connected = False
-
-# Construct connection string
-# try:
-#    conn = mysql.connector.connect(**config)
-#    print("Connection established")
-# except mysql.connector.Error as err:
-#   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-#     print("Something is wrong with the user name or password")
-#   elif err.errno == errorcode.ER_BAD_DB_ERROR:
-#     print("Database does not exist")
-#   else:
-#     print(err)
-# else:
-#   is_connected = True
-#   cursor = conn.cursor()
 
 
 def createTables():
@@ -35,8 +18,7 @@ def createTables():
     return False
   else:
     cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
-    # cursor.execute("CREATE TABLE IF NOT EXISTS FacesDetectedInfo (id serial PRIMARY KEY, url text, result text);")
+    cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
     conn.commit()
     cursor.close()
     conn.close()
